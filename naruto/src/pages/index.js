26ef88b20dee78ef26acd6422350b11bd5ac88fb
Priscilla Van der Schoot <priscilla.vanderschoot@student.ap.be>
2023-01-08 09:44:54 +0100

    langlopende taak

---
+++ naruto/src/pages/index.js
@@ -0,0 +1,13 @@
+// Stap 1: Importeer React
+import * as React from 'react'
+import Layout from '../components/layout'
+// Stap 2: definieer je component
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Naruto">
+      <p>Lorem ipsum</p>
+      </Layout>
+  )
+}
+// Stap 3: Exporteer je component
+export default IndexPage
